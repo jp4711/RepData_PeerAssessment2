39d47c48ac8d7e29c73d7a4cfee171e602a3266d
JP <johann.posch@gmail.com>
2014-07-27 07:59:37 -0700

    first completed version

---
+++ Tests1.R
@@ -0,0 +1,9 @@
+#install.packages(c("rj", "rj.gd"), repos="http://download.walware.de/rj-2.0")
+#p <- ggplot(X, aes(x=STATE,y=totalDmg)) + 
+#     geom_bar(stat="identity", fill="lightblue", colour="black") +
+#     ggtitle("Top Damage By State") +
+#     ylab("Damage $ Millions") +
+#     xlab("State")
+#p
+#levels(X)
+#attributes(X)
